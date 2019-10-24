63f98973cee774cdbc8e56331b149346e920ea9a
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-10-25 00:33:27 +0900

    impl: read files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::env;
-use std::io::{self, BufRead};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 fn main() {
-    for argument in env::args() {
-        println!("argument: {}", argument)
+    for filename in env::args() {
+        if read_file(&filename).is_err() {
+            println!("file not found");
+        }
     }
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap());
     }
+}
+
+fn read_file(filename: &str) -> std::io::Result<()> {
+    let file = File::open(filename)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    println!("{}", contents);
+    Ok(())
 }
