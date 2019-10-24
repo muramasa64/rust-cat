01eb7f6a9b56a0d9c49e404642b11abc2363f6ba
ISOBE Kazuhiko <muramasa64@gmail.com>
2019-10-25 00:02:24 +0900

    impl: example for env::args()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::env;
 use std::io::{self, BufRead};
 
 fn main() {
+    for argument in env::args() {
+        println!("argument: {}", argument)
+    }
+
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap());
